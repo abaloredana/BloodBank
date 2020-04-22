7e2dd60bd49ad17a02d9861050fa4c2408104cc0
abaloredana <loredanabalo1006@gmail.com>
2020-04-22 22:07:29 +0200

    Properties, getters and setters for all classes except UI.java

--- src/LINKERS/BloodSample.java
+++ src/LINKERS/BloodSample.java
@@ -1,5 +1,67 @@
 package LINKERS;
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;  
 
 public class BloodSample {
+	 
+	public int id;
+	public int hematocrit;
+	public static int volume = 450;
+	public int plasma;
+	public int platelets;
+	public int hdl;
+	public int haemoglobin;
+	public LocalDateTime dod;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getHematocrit() {
+		return hematocrit;
+	}
+	public void setHematocrit(int hematocrit) {
+		this.hematocrit = hematocrit;
+	}
+	public static int getVolume() {
+		return volume;
+	}
+	public static void setVolume(int volume) {
+		BloodSample.volume = volume;
+	}
+	public int getPlasma() {
+		return plasma;
+	}
+	public void setPlasma(int plasma) {
+		this.plasma = plasma;
+	}
+	public int getPlatelets() {
+		return platelets;
+	}
+	public void setPlatelets(int platelets) {
+		this.platelets = platelets;
+	}
+	public int getHdl() {
+		return hdl;
+	}
+	public void setHdl(int hdl) {
+		this.hdl = hdl;
+	}
+	public int getHaemoglobin() {
+		return haemoglobin;
+	}
+	public void setHaemoglobin(int haemoglobin) {
+		this.haemoglobin = haemoglobin;
+	}
+	public LocalDateTime getDod() {
+		return dod;
+	}
+	public void setDod(LocalDateTime dod) {
+		this.dod = dod;
+	}  
+	
+	
 
 }
---
+++ src/PASSIVES/Donor.java
@@ -0,0 +1,102 @@
+package PASSIVES;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter; 
+
+public class Donor {
+	
+	public int id;
+	public String name;
+	public String surname;
+	public String email;
+	public String phone;
+	public String address;
+	public String city;
+	public String zip;
+	public float weight;
+	public String gender;
+	public String abo;
+	public String rh;
+	public LocalDateTime dob;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAbo() {
+		return abo;
+	}
+	public void setAbo(String abo) {
+		this.abo = abo;
+	}
+	public String getRh() {
+		return rh;
+	}
+	public void setRh(String rh) {
+		this.rh = rh;
+	}
+	public LocalDateTime getDob() {
+		return dob;
+	}
+	public void setDob(LocalDateTime dob) {
+		this.dob = dob;
+	}
+
+}
