580dcca7c65a84a38e3a1a2a6445852eb1e989d5
abaloredana <loredanabalo1006@gmail.com>
2020-04-22 20:49:51 +0200

    Added properties to BB_USER

---
+++ src/BB_USER/BloodBank.java
@@ -0,0 +1,12 @@
+package BB_USER;
+
+public class BloodBank {
+	public Integer id;
+	public String email;
+	public String address;
+	public String phone;
+	public Integer capacity;
+	public String city;
+	public String zip;
+
+}
--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,5 +1,6 @@
 package UI;
 
 public class Main {
+	
 
 }
