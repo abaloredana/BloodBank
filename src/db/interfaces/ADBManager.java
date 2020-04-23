67c01d25ea747b271b723c96a6929fb5fd613f20
andre <andre@LAPTOP-HOJOFRVH>
2020-04-23 20:55:41 +0200

    updates

---
+++ src/db/interfaces/ADBManager.java
@@ -0,0 +1,8 @@
+package db.interfaces;
+
+public interface ADBManager {
+	
+	public void connect();
+	public void disconnect();
+	public void createTables();
+}
---
+++ src/db/sqlite/SQLiteManager.java
@@ -0,0 +1,25 @@
+package db.sqlite;
+
+import db.interfaces.ADBManager;
+
+public class SQLiteManager implements ADBManager {
+
+	@Override
+	public void connect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void createTables() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
